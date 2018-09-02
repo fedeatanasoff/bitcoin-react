6aaa19f0e87c3eae4485c71a279f7e353a54b674
fd <fede.atanasoff@gmail.com>
2018-09-02 11:35:20 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Header from "./components//common/Header";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>desde App</h2>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
