cf96976179c814140e5eef0bab6ac3a516f8b87e
fd <fede.atanasoff@gmail.com>
2018-09-14 19:59:39 -0300

    empezando con el detalle de moneda. agregando un onclick a tr de table

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { API_URL } from "../../config";
 import "./Detail.css";
 
 class Detail extends Component {
+  componentDidMount() {
+    const currencyId = this.props.match.params.id;
+    console.log(currencyId);
+
+    fetch(`${API_URL}/cryptocurrencies/${currencyId}`)
+      .then(response => {
+        return response.json().then(json => {
+          return response.ok ? json : Promise.reject(json);
+        });
+      })
+      .then(data => console.log(data))
+      .catch(error => {
+        console.log("error", error);
+      });
+  }
+
   render() {
     return (
       <div>
