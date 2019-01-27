044426607c57f5060ab237d63f9d1f447f2422ce
fedeatanasoff <fede.atanasoff@gmail.com>
2019-01-27 19:11:46 -0300

    search con loading y resultados en consola

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,21 +1,43 @@
 import React, { Component } from "react";
-
+import { handleResponse } from "../../helper";
+import { API_URL } from "../../config";
+import "./Search.css";
 export class Search extends Component {
   state = {
-    searchQuery: ""
+    searchQuery: "",
+    loading: false
   };
 
   handleChange = event => {
     let inputValue = event.target.value;
     this.setState({ searchQuery: inputValue });
-    console.log(this.state);
+
+    if (!inputValue) {
+      console.log("input vacio");
+      return "";
+    }
+
+    this.setState({ loading: true });
+
+    fetch(`${API_URL}/autocomplete?searchQuery=${inputValue}`)
+      .then(handleResponse)
+      .then(data => {
+        console.log(data);
+        this.setState({ loading: false });
+      });
   };
 
   render() {
     return (
-      <form className="form-inline my-2 my-lg-0">
-        <input onChange={this.handleChange} />
-      </form>
+      <div className="form-inline my-2 my-lg-0">
+        <i className="fas fa-search-dollar" />
+        <input onChange={this.handleChange} placeholder="buscar moneda" />
+        {this.state.loading && (
+          <div className="search-loading">
+            <i className="fas fa-spinner fa-spin load" />
+          </div>
+        )}
+      </div>
     );
   }
 }
