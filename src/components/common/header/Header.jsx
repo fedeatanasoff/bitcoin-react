ff088118332d8944b39a6115a01deff2a281edc3
fedeatanasoff <fede.atanasoff@gmail.com>
2019-01-27 02:25:10 -0300

    cambios en el logo e incluyendo el redirects para el not founds

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 class Header extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
+        <Link to="/" className="navbar-brand">
+          <i class="fab fa-react fa-spin rank" />
+          &nbsp; ReactCoins
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -22,26 +24,11 @@ class Header extends Component {
 
         <div className="collapse navbar-collapse" id="navbarColor03">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
+            {/* <li className="nav-item active">
               <a className="nav-link" href="#">
                 Home <span className="sr-only">(current)</span>
               </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Features
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Pricing
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                About
-              </a>
-            </li>
+            </li> */}
           </ul>
           <div className="form-inline my-2 my-lg-0">
             <input
